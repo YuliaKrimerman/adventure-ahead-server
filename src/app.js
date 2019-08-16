06ae10c205c14f1b2688244ae0d7267ae9dbe616
Yulia Krimerman <juliapiterova@hotmail.com>
2019-08-16 15:07:42 -0400

    make it better

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
-const { CLIENT_ORIGIN } = require('./config')
+
 const travelRouter = require('./travelData/travel-data-router');
 const packRouter = require('./packData/pack-data-router');
 const usersRouter = require('./usersData/user-router');
--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DATABASE_URL || 'postgres://vexeyxonmgpnev:04b6cafc81b063b5193de2f8949ed189d9d571c406b7e0965e29044bdec7e478@ec2-23-21-186-85.compute-1.amazonaws.com:5432/d30pnkhh3skp4r',
+    DB_URL: process.env.DATABASE_URL || 'postgresql://dunder-mifflin:123@localhost/adventure',
 	CLIENT_ORIGIN:process.env.CLIENT_ORIGIN || 3000,
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '3h'
