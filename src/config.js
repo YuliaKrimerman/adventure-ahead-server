c94c765705ff364eb3dcb0dd5745b7de698aaab2
Yulia Krimerman <juliapiterova@hotmail.com>
2019-08-16 11:12:05 -0400

    changes

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DATABASE_URL || 'postgres://lstenncndthnyl:5d680ef8fc125afe1854b5437d0910d2bd802bdabaa8fbfbd58acdb70f21254e@ec2-75-101-131-79.compute-1.amazonaws.com:5432/ddd831l7m7theb',
+    DB_URL: process.env.DATABASE_URL || 'postgresql://dunder-mifflin:123@localhost/adventure',
 	 CLIENT_ORIGIN:process.env.CLIENT_ORIGIN || 3000,
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '3h'
